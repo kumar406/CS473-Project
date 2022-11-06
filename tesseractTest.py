e13610eb1ef39713801796c02adafb349aca0f13
Dithi Saxena <saxena52@purdue.edu>
2022-11-06 16:49:44 -0500

    psuedo input test from module1

--- tesseractTest.py
+++ tesseractTest.py
@@ -4,7 +4,21 @@ from PIL import Image
 
 
 img = Image.open(r"Collection_1/001.png")
-cropped_img = img.crop((6, 13, 1628, 1129))
+cropped_img = img.crop((2674, 1, 2993, 181))
 cropped_img = cropped_img.convert('RGB')
 cropped_img.save("cropped_img.jpg")
-print(pytesseract.image_to_string(cropped_img))
+print(pytesseract.image_to_string(cropped_img, lang='eng', config='--psm 6'))
+
+# input list
+# img = Image.open(r"input file")
+dict = {"entity": [1, 313, 723, 676], "rel_attr":[757, 1, 1076, 186], "rel":[1001, 330, 1323, 659], "entity":[2935, 310, 3652, 669]}
+arr = []
+for x in dict:
+    temp = []
+    cropped_img = img.crop(dict[x])
+    cropped_img = cropped_img.convert('RGB')
+    temp.append(x)
+    temp.append(pytesseract.image_to_string(cropped_img).strip())
+    arr.append(temp)
+
+print(arr)
